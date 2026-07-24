fea3217adaae06bbfa15205657b15473c51e2844
Andrey <aprishlov@gmail.com>
2026-07-24 10:30:11 +0300

    feat: initialize project

---
+++ src/test/java/com/itmentorcommunityplatform/intervalrepetitionservice/IntervalRepetitionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itmentorcommunityplatform.intervalrepetitionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntervalRepetitionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
