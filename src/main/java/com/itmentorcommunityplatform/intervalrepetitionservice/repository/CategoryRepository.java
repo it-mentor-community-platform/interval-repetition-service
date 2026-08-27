4d9d77e7c1ad3577f3d456245c61c7b5141cb5b1
Andrey <aprishlov@gmail.com>
2026-08-28 00:03:12 +0300

    feat: create specialization flow

--- src/main/java/com/itmentorcommunityplatform/intervalrepetitionservice/repository/CategoryRepository.java
+++ src/main/java/com/itmentorcommunityplatform/intervalrepetitionservice/repository/CategoryRepository.java
@@ -3,10 +3,13 @@ package com.itmentorcommunityplatform.intervalrepetitionservice.repository;
 import com.itmentorcommunityplatform.intervalrepetitionservice.entity.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CategoriesRepository extends CrudRepository<Category, Long> {
 
     Optional<Category> findByNameAndSpecializationId(String name, Long specializationId);
+
+    List<Category> findBySpecializationId(Long specializationId);
 
 }
