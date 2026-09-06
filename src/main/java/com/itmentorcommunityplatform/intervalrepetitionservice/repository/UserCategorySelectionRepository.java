727b1d83707c61c59b4e7a8739cb9515b2152e7c
Andrey <aprishlov@gmail.com>
2026-09-06 18:42:35 +0300

    fix: add query

--- src/main/java/com/itmentorcommunityplatform/intervalrepetitionservice/repository/UserCategorySelectionRepository.java
+++ src/main/java/com/itmentorcommunityplatform/intervalrepetitionservice/repository/UserCategorySelectionRepository.java
@@ -1,12 +1,19 @@
 package com.itmentorcommunityplatform.intervalrepetitionservice.repository;
 
 import com.itmentorcommunityplatform.intervalrepetitionservice.entity.UserCategorySelection;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.Set;
+import java.util.List;
+
 
 public interface UserCategorySelectionRepository extends CrudRepository<UserCategorySelection, Long> {
 
-    Set<Long> findCategoryIdsByUserId(Long userId);
+    @Query("""
+                SELECT category_id
+                FROM user_category_selections
+                WHERE user_id = :userId
+            """)
+    List<Long> findCategoryIdsByUserId(Long userId);
 
 }
