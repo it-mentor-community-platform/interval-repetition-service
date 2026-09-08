24cd1cd67d827d210456877b6662624c02b37dac
Andrey <aprishlov@gmail.com>
2026-09-08 11:11:32 +0300

    refactor: remove user flow

--- src/main/java/com/itmentorcommunityplatform/intervalrepetitionservice/validator/HeaderValidator.java
+++ src/main/java/com/itmentorcommunityplatform/intervalrepetitionservice/validator/HeaderValidator.java
@@ -3,27 +3,14 @@ package com.itmentorcommunityplatform.intervalrepetitionservice.validator;
 import com.itmentorcommunityplatform.intervalrepetitionservice.exception.ValidationException;
 
 
-import java.util.List;
-
 public class HeaderValidator {
 
     private HeaderValidator() {}
 
-    public static void validateNameHeader(String name) {
-        if (name == null || name.isBlank()) {
-            throw new ValidationException("Access denied: missing name header");
-        }
-    }
 
     public static void validateIdHeader(Long id) {
         if (id == null) {
             throw new ValidationException("Access denied: missing id header");
-        }
-    }
-
-    public static void validateRoleHeader(List<String> roles) {
-        if (roles == null || roles.isEmpty()) {
-            throw new ValidationException("Access denied: missing role header");
         }
     }
 
