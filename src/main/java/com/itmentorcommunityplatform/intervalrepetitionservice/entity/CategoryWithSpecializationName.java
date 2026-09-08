1e6041c75440e03cbefe1d0fbb7eaef17c8ff9e5
Andrey <aprishlov@gmail.com>
2026-09-08 13:03:28 +0300

    refactor: removed builder

--- src/main/java/com/itmentorcommunityplatform/intervalrepetitionservice/entity/CategoryWithSpecializationName.java
+++ src/main/java/com/itmentorcommunityplatform/intervalrepetitionservice/entity/CategoryWithSpecializationName.java
@@ -1,7 +1,6 @@
 package com.itmentorcommunityplatform.intervalrepetitionservice.entity;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,7 +9,6 @@ import org.springframework.data.relational.core.mapping.Table;
 
 @Getter
 @Setter
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Table("categories")
