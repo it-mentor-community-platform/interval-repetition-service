a5b4e217f6dc0ccc3b7904de661ab3f2cd594a49
Andrey <aprishlov@gmail.com>
2026-09-08 20:12:32 +0300

    refactor: removed useless query

--- src/main/java/com/itmentorcommunityplatform/intervalrepetitionservice/repository/UserCategorySelectionRepository.java
+++ src/main/java/com/itmentorcommunityplatform/intervalrepetitionservice/repository/UserCategorySelectionRepository.java
@@ -1,20 +1,12 @@
 package com.itmentorcommunityplatform.intervalrepetitionservice.repository;
 
 import com.itmentorcommunityplatform.intervalrepetitionservice.entity.UserCategorySelection;
-import org.springframework.data.jdbc.repository.query.Modifying;
-import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 
 
 
 public interface UserCategorySelectionRepository extends CrudRepository<UserCategorySelection, Long> {
 
-    @Modifying
-    @Query("""
-            DELETE FROM user_category_selections
-            WHERE user_id = :userId
-                  AND category_id = :categoryId 
-            """)
     void deleteByUserIdAndCategoryId(Long userId, Long categoryId);
 
 }
