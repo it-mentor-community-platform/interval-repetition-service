abd624304d16594de6ae3455a00c14f39a9debf8
Andrey <aprishlov@gmail.com>
2026-09-08 19:35:38 +0300

    refactor: move selected categories mapping to another class

---
+++ src/main/java/com/itmentorcommunityplatform/intervalrepetitionservice/mapper/SelectedCategoryMapper.java
@@ -0,0 +1,27 @@
+package com.itmentorcommunityplatform.intervalrepetitionservice.mapper;
+
+import com.itmentorcommunityplatform.intervalrepetitionservice.dto.SavedSelectedCategoryResponseDto;
+import com.itmentorcommunityplatform.intervalrepetitionservice.dto.SelectedCategoryResponseDto;
+import com.itmentorcommunityplatform.intervalrepetitionservice.entity.CategoryWithSpecializationName;
+import com.itmentorcommunityplatform.intervalrepetitionservice.model.CategoryWithStatistics;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface SelectedCategoryMapper {
+
+    @Mapping(target = "specialization", source = "category.specializationName")
+    SavedSelectedCategoryResponseDto toSavedSelectedCategoryResponse(CategoryWithSpecializationName category);
+
+    List<SavedSelectedCategoryResponseDto> toSavedSelectedCategoryResponseList(List<CategoryWithSpecializationName> categories);
+
+
+    @Mapping(target = "specialization", source = "category.specializationName")
+    SelectedCategoryResponseDto toSelectedCategoryResponse(CategoryWithStatistics category);
+
+    List<SelectedCategoryResponseDto> toSelectedCategoryResponseList(List<CategoryWithStatistics> categories);
+
+
+}
