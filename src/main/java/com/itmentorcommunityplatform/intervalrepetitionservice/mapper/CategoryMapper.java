90b46f662d49b766f650a5e57eae4d7bc08fb60b
Andrey <aprishlov@gmail.com>
2026-09-08 19:35:06 +0300

    refactor: move selected categories mapping to another class

--- src/main/java/com/itmentorcommunityplatform/intervalrepetitionservice/mapper/CategoryMapper.java
+++ src/main/java/com/itmentorcommunityplatform/intervalrepetitionservice/mapper/CategoryMapper.java
@@ -1,12 +1,8 @@
 package com.itmentorcommunityplatform.intervalrepetitionservice.mapper;
 
 import com.itmentorcommunityplatform.intervalrepetitionservice.dto.CategoryResponseDto;
-import com.itmentorcommunityplatform.intervalrepetitionservice.dto.SavedSelectedCategoryResponseDto;
-import com.itmentorcommunityplatform.intervalrepetitionservice.dto.SelectedCategoryResponseDto;
-import com.itmentorcommunityplatform.intervalrepetitionservice.entity.CategoryWithSpecializationName;
 import com.itmentorcommunityplatform.intervalrepetitionservice.model.CategoryWithStatistics;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 import java.util.List;
 
@@ -16,19 +12,5 @@ public interface CategoryMapper {
     CategoryResponseDto toResponse(CategoryWithStatistics category);
 
     List<CategoryResponseDto> toResponseList(List<CategoryWithStatistics> categories);
-
-    @Mapping(target = "specialization", source = "category.specializationName")
-    SavedSelectedCategoryResponseDto toSavedSelectedCategoryResponse(CategoryWithSpecializationName category);
-
-    List<SavedSelectedCategoryResponseDto> toSavedSelectedCategoryResponseList(
-            List<CategoryWithSpecializationName> categories);
-
-
-    @Mapping(target = "specialization", source = "category.specializationName")
-    SelectedCategoryResponseDto toSelectedCategoryResponse(CategoryWithStatistics category);
-
-
-    List<SelectedCategoryResponseDto> toSelectedCategoryResponseList(
-            List<CategoryWithStatistics> categories);
 
 }
